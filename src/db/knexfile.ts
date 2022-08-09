2fda94f1598d6ddf419901f8d9a87ca650e597f8
Oshin Gansi <os.gansi@gmail.com>
2022-08-09 10:47:23 +0545

    fix error

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -16,11 +16,11 @@ const config: { [key: string]: Knex.Config } = {
 
 
   development: {
-    client: "postgres",
+    client: "process.env.DB_CLIENT",
     connection: {
-      database: "vas",
-      user: "oshing",
-      password: "oshin123"
+      database: "process.env.DB_NAME",
+      user: "process.env.DB_USER",
+      password: "process.env.DB_PASSWORD"
     },
     pool: {
       min: 2,
